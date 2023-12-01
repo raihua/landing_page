d2fb8e81aec474d3bc736c646307a79fec94c063
raihua <raihua.dev@gmail.com>
2023-12-01 16:00:37 +1030

    Attempt to fix fetch error

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -3,16 +3,25 @@ import { Link } from "react-router-dom";
 const Record = (props) => (
  <tr>
    <td>{props.record.name}</td>
-   <td>{props.record.email}</td>
+   <td>{props.record.Email}</td>
+   <td>
+     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
+     <button className="btn btn-link"
+       onClick={() => {
+         props.deleteRecord(props.record._id);
+       }}
+     >
+       Delete
+     </button>
+   </td>
  </tr>
 );
-
 export default function RecordList() {
  const [records, setRecords] = useState([]);
   // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`http://localhost:3000/record/`);
+     const response = await fetch(`http://localhost:5000/record/`);
       if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
        window.alert(message);
@@ -24,7 +33,26 @@ export default function RecordList() {
     getRecords();
     return;
  }, [records.length]);
-
+  // This method will delete a record
+ async function deleteRecord(id) {
+   await fetch(`http://localhost:5000/${id}`, {
+     method: "DELETE"
+   });
+    const newRecords = records.filter((el) => el._id !== id);
+   setRecords(newRecords);
+ }
+  // This method will map out the records on the table
+ function recordList() {
+   return records.map((record) => {
+     return (
+       <Record
+         record={record}
+         deleteRecord={() => deleteRecord(record._id)}
+         key={record._id}
+       />
+     );
+   });
+ }
   // This following section will display the table with the records of individuals.
  return (
    <div>
